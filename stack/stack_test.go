2250c63f5b9418e65163b5dac738f7134baa7b6a
Raphael Rossi <raphael.rossi@stone.com.br>
2024-06-18 20:25:50 -0300

    rename package name

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -3,66 +3,66 @@ package stack_test
 import (
 	"testing"
 
-	Stack "github.com/RaphaelVRossi/golang-stack-tdd/stack"
+	"github.com/RaphaelVRossi/golang-stack-tdd/stack"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNew(t *testing.T) {
-	stack := Stack.New()
-	assert.NotNil(t, stack)
-	assert.True(t, stack.IsEmpty())
+	s := stack.New()
+	assert.NotNil(t, s)
+	assert.True(t, s.IsEmpty())
 }
 
 func Test_Push_IsNotEmpty(t *testing.T) {
-	stack := Stack.New()
-	stack.Push(1)
-	assert.False(t, stack.IsEmpty())
-	assert.Equal(t, 1, stack.GetSize())
+	s := stack.New()
+	s.Push(1)
+	assert.False(t, s.IsEmpty())
+	assert.Equal(t, 1, s.GetSize())
 }
 
 func Test_Push_Pop_isEmpty(t *testing.T) {
-	stack := Stack.New()
-	stack.Push(1)
-	stack.Pop()
-	assert.True(t, stack.IsEmpty())
-	assert.Equal(t, 0, stack.GetSize())
+	s := stack.New()
+	s.Push(1)
+	s.Pop()
+	assert.True(t, s.IsEmpty())
+	assert.Equal(t, 0, s.GetSize())
 }
 
 func Test_Push_GetSize_IsTwo(t *testing.T) {
-	stack := Stack.New()
-	stack.Push(0)
-	stack.Push(0)
-	assert.Equal(t, 2, stack.GetSize())
+	s := stack.New()
+	s.Push(0)
+	s.Push(0)
+	assert.Equal(t, 2, s.GetSize())
 }
 
 func Test_Pop_UnderflowError(t *testing.T) {
-	stack := Stack.New()
-	_, err := stack.Pop()
+	s := stack.New()
+	_, err := s.Pop()
 
 	assert.Error(t, err)
 }
 
 func Test_Push_X_Pop_X(t *testing.T) {
-	stack := Stack.New()
+	s := stack.New()
 
-	stack.Push(99)
-	element, _ := stack.Pop()
+	s.Push(99)
+	element, _ := s.Pop()
 	assert.Equal(t, 99, element)
 
-	stack.Push(88)
-	element, _ = stack.Pop()
+	s.Push(88)
+	element, _ = s.Pop()
 	assert.Equal(t, 88, element)
 }
 
 func Test_Push_XY_Pop_YX(t *testing.T) {
-	stack := Stack.New()
+	s := stack.New()
 
-	stack.Push(99)
-	stack.Push(88)
+	s.Push(99)
+	s.Push(88)
 
-	element, _ := stack.Pop()
+	element, _ := s.Pop()
 	assert.Equal(t, 88, element)
 
-	element, _ = stack.Pop()
+	element, _ = s.Pop()
 	assert.Equal(t, 99, element)
 }
