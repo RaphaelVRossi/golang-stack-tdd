c3aba59b766f366781b41c377c55dca59ddad1a2
Raphael Rossi <raphael.rossi@stone.com.br>
2024-09-05 21:00:28 -0300

    aaa

--- stack/stack.go
+++ stack/stack.go
@@ -1,39 +1,22 @@
 package stack
 
 type Stack struct {
-	empty    bool
-	size     int
-	elements []int
-}
-
-type UnderflowError struct{}
-
-func (u *UnderflowError) Error() string {
-	return "can not pop a empty stack"
+	empty bool
 }
 
 func New() *Stack {
-	return &Stack{size: 0, elements: make([]int, 10)}
+	return &Stack{empty: true}
 }
 
 func (s *Stack) IsEmpty() bool {
-	return s.size == 0
-}
-
-func (s *Stack) GetSize() int {
-	return s.size
+	return s.empty
 }
 
 func (s *Stack) Push(element int) {
-	s.elements[s.size] = element
-	s.size++
+	s.empty = false
 }
 
-func (s *Stack) Pop() (int, error) {
-	if s.size == 0 {
-		return -1, &UnderflowError{}
-	}
-
-	s.size--
-	return s.elements[s.size], nil
+func (s *Stack) Pop() int {
+	s.empty = true
+	return 1
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -7,62 +7,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNothing(t *testing.T) {
+	assert.True(t, 1 == 1)
+}
+
 func TestNew(t *testing.T) {
 	s := stack.New()
-	assert.NotNil(t, s)
 	assert.True(t, s.IsEmpty())
 }
 
 func TestPushIsNotEmpty(t *testing.T) {
 	s := stack.New()
+
 	s.Push(1)
 	assert.False(t, s.IsEmpty())
-	assert.Equal(t, 1, s.GetSize())
 }
 
-func TestPushPopisEmpty(t *testing.T) {
+func TestPushPopIsEmpty(t *testing.T) {
 	s := stack.New()
+
 	s.Push(1)
-	s.Pop()
+	e := s.Pop()
+
 	assert.True(t, s.IsEmpty())
-	assert.Equal(t, 0, s.GetSize())
-}
-
-func TestPushGetSizeIsTwo(t *testing.T) {
-	s := stack.New()
-	s.Push(0)
-	s.Push(0)
-	assert.Equal(t, 2, s.GetSize())
-}
-
-func TestPopUnderflowError(t *testing.T) {
-	s := stack.New()
-	_, err := s.Pop()
-
-	assert.Error(t, err)
-}
-
-func TestPushXPopX(t *testing.T) {
-	s := stack.New()
-
-	s.Push(99)
-	element, _ := s.Pop()
-	assert.Equal(t, 99, element)
-
-	s.Push(88)
-	element, _ = s.Pop()
-	assert.Equal(t, 88, element)
-}
-
-func TestPushXYPopYX(t *testing.T) {
-	s := stack.New()
-
-	s.Push(99)
-	s.Push(88)
-
-	element, _ := s.Pop()
-	assert.Equal(t, 88, element)
-
-	element, _ = s.Pop()
-	assert.Equal(t, 99, element)
+	assert.Equal(t, 1, e)
 }
